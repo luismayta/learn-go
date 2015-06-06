0cd045fc95b4c2f8bfcad990470bc808c830eda8
@slovacus <slovacus@gmail.com>
2015-06-06 00:43:16 -0500

    [feature/variables] added example constants

--- variables/constants.go
+++ variables/constants.go
@@ -4,6 +4,13 @@ import "fmt"
 
 const Pi = 3.1416
 
+const (
+    StatusOk = 200
+    StatusNotFound = 404
+)
+
 func main() {
     fmt.Printf("Value Pi %f", Pi)
+    fmt.Printf("Value StatusOk %d", StatusOk)
+    fmt.Printf("Value StatusNotFound %d", StatusNotFound)
 }
