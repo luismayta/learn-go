ddb786f270a8302ac7f801c760d32ed3687de73a
Luis Mayta <luis@hadenlabs.com>
2015-09-05 17:36:52 -0500

    sample: add example iota (#1)

---
+++ constants/iota.go
@@ -0,0 +1,18 @@
+// Examples with iota
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	pi = 3.1416
+	piTambien
+	igualApi
+)
+
+func main() {
+	fmt.Println(pi)
+	fmt.Println(piTambien)
+	fmt.Println(igualApi)
+}
