cfa20fd637bd9325bbce709a6b993040d9b7cc6e
Luis Mayta <luis@hadenlabs.com>
2015-09-05 17:30:01 -0500

    fix: order syntax code (#1)

--- func/example1.go
+++ func/example1.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-func add (x , y int) int {
-    return x + y
+func add(x, y int) int {
+	return x + y
 }
 
 func main() {
-    fmt.Println(add(10, 10))
+	fmt.Println(add(10, 10))
 }
