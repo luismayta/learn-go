acb377e0947d2bf47b30429be2744b2ae425ab7d
Luis Mayta <luis@hadenlabs.com>
2016-05-30 18:34:53 -0500

    chore: update path files golang (#5)

---
+++ advanced/goroutines/yield-best.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func fib (n int) chan int {
+	C := make(chan int)
+
+	go func(){
+		a, b := 0, 1
+		for i :=0; i < n; i++ {
+			a, b = b, a + b
+			C <- a
+		}
+		close(C)
+	}()
+	return C
+}
+
+
+func main() {
+	for x := range(fib(10)){
+		fmt.Println(x)
+	}
+}
