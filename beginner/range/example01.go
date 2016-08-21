e41b5997d47f73669acc8466ff6fd5daf69f9dc9
@slovacus <slovacus@gmail.com>
2016-08-20 21:01:31 -0500

    sample: range read (#14)

---
+++ beginner/range/example01.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums := []int{1, 2, 3}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("Index:", i)
+		}
+	}
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
+}
