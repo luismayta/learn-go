5929123e0aa32ead9800eb8ca5b420353fa34eb0
@slovacus <slovacus@gmail.com>
2016-08-20 20:08:38 -0500

    sample: update example map (#12)

--- beginner/map/example2.go
+++ beginner/map/example2.go
@@ -9,4 +9,13 @@ func main() {
 	m["k1"] = 7
 	m["k2"] = 7
 	fmt.Println("map:", m)
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+	fmt.Println("len:", len(m))
+	delete(m, "k2")
+	fmt.Println("map:", m)
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
 }
