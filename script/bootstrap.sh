eebb1fbe1cf1ac307a325277c550d7afe890a301
@slovacus <slovacus@gmail.com>
2016-10-08 02:18:49 -0500

    chore: implement script make (#17)

---
+++ script/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+PROJECT_NAME=go_examples
+PYTHON_VERSION=2.7.9
+PYENV_NAME="${PROJECT_NAME}"
+
+GVM_NAME="${PROJECT_NAME}"
+GVM_PATHS_NAME="{src, pkg, bin}"
+
+# Vars Dir
+ROOT_DIR="`pwd`"
+RESOURCES_DIR="$ROOT_DIR/resources"
+RESOURCES_DB_DIR="$RESOURCES_DIR/db"
---
+++ script/pyenv.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+# load source files externals
+if [ -e "$HOME/.pyenv" ]; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+
+cd "${ROOT_DIR}"
+
+pyenv versions | grep -q "${PYENV_NAME}"
+response=$?
+
+if [[ ! ${response} -eq '0' ]]; then
+    pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" >> /dev/null 2>&1
+fi
