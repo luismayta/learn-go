fa4650dc4ccdfb5b81e4ef322c2cf4da0ea0364e
Luis Mayta <luis@hadenlabs.com>
2017-04-03 15:09:41 -0500

    fix: settings script bash (#19)

--- script/pyenv.sh
+++ script/pyenv.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+# shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 # load source files externals
@@ -9,13 +10,6 @@ if [ -e "$HOME/.pyenv" ]; then
     eval "$(pyenv virtualenv-init -)"
 fi
 
-cd "${ROOT_DIR}"
+cd "${ROOT_DIR}" || exit
 
-pyenv versions | grep -q "${PYENV_NAME}"
-response=$?
-
-if [[ ! ${response} -eq '0' ]]; then
-    pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" >> /dev/null 2>&1
-fi
-
-pyenv activate "${PROJECT_NAME}" >> /dev/null 2>&1
+pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" >> /dev/null 2>&1 || echo 'Oh Yeah!!'
