4dd3ecc5571a811c1cc7445daf1261577847a431
@slovacus <slovacus@gmail.com>
2017-04-03 15:30:25 -0500

    !#19 Fix: settings bootstrap

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-PROJECT_NAME=go_examples
-PYTHON_VERSION=2.7.9
-PYENV_NAME="${PROJECT_NAME}"
+export PROJECT_NAME=golang-examples
 
-GVM_NAME="${PROJECT_NAME}"
-GVM_PATHS_NAME="{src, pkg, bin}"
+export PYTHON_VERSION=2.7.9
+export PYENV_NAME="${PROJECT_NAME}"
 
-RVM_NAME="${PROJECT_NAME}"
-RVM_VERSION=2.1.0
+export GVM_NAME="${PROJECT_NAME}"
+export GVM_PATHS_NAME=(
+    "src"
+    "pkg"
+    "bin"
+)
+
+export GRIP_PORT=6430
+export DEPLOY_ACCOUNT=ubuntu
+
 # Vars Dir
-ROOT_DIR="`pwd`"
-RESOURCES_DIR="$ROOT_DIR/resources"
-RESOURCES_DB_DIR="$RESOURCES_DIR/db"
+export ROOT_DIR
+ROOT_DIR=$(pwd)
+export RESOURCES_DIR="$ROOT_DIR/resources"
+export RESOURCES_DB_DIR="$RESOURCES_DIR/db"
+export REQUIREMENTS_DIR="${ROOT_DIR}/requirements/"
+export PROVISION_DIR="$ROOT_DIR/provision/ansible"
+export SOURCE_DIR="${ROOT_DIR}"
 
-source "${ROOT_DIR}/.env" >> /dev/null 2>&1
+
