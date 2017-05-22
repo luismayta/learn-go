75b88b6fa38c3a3a9f466d4cb43e414c12ca9899
Luis Mayta <luis@hadenlabs.com>
2017-05-22 14:49:14 -0500

    fix: order code go (#24)

--- beginner/arrays/example1.go
+++ beginner/arrays/example1.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
 )
 
 var users = [...]string{"Luis", "Jorge", "Oscar"}
 
 func main() {
-	fmt.Println(users)
-	for _, value := range users {
-		fmt.Println(value)
-	}
+  fmt.Println(users)
+  for _, value := range users {
+    fmt.Println(value)
+  }
 }
