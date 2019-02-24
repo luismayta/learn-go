e100b91f28d7a25a658d8adb70a5f33d8b6abb43
Luis Mayta <luis@hadenlabs.com>
2019-02-24 10:03:19 -0500

    fix: unnecessary files (#23)

--- script/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# shellcheck source=/dev/null
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-pyenv activate "${PYENV_NAME}" >> /dev/null 2>&1 || echo 'Oh Yeah!!'
-pip install -r "${REQUIREMENTS_DIR}/dev.txt"
