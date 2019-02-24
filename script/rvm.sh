d87d823aa9cdfb1c657a3c1521c1061e45f313bc
@slovacus <slovacus@gmail.com>
2019-02-24 10:03:19 -0500

    (#23) Fix: unnecesary files

--- script/rvm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-rvm gemset create "${RVM_NAME}" >> /dev/null 2>&1
-rvm use "${RVM_VERSION}"@"${RVM_NAME}" >> /dev/null 2>&1
