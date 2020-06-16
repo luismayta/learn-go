587004484c259eda074ab5a060d87705dbb994f2
@slovacus <slovacus@gmail.com>
2020-06-16 06:25:12 -0500

    chore: refactor actions for make (#26)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -1,20 +1,22 @@
 test.help:
 	@echo '    Tests:'
 	@echo ''
-	@echo '        test                      show help'
-	@echo '        test.all   	             Run all module test'
-	@echo '        test.lint                 Run all pre-commit'
+	@echo '        test                    Run all test'
+	@echo '        test.run                Run test of test'
+	@echo '        test.lint               Run all pre-commit'
+	@echo '        test.syntax             Run all syntax in code'
 	@echo ''
 
 test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
 	@if [ -z "${run}" ]; then \
-		make test.help;\
+		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest"; \
+	elif [ -n "${run}" ]; then \
+		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest tests/${run}"; \
 	fi
-
-test.all:
-	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	$(docker-test-run) bash -c "zunit --verbose"
 
 test.lint:
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
+
+test.syntax:
+	@echo $(MESSAGE) Running tests $(END)
