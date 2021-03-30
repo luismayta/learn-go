1ee22a4b7a7ff4e9df53759c6c2a8c23300b99c4
@slovacus <slovacus@gmail.com>
2021-03-30 14:31:38 -0500

    refactor: actions make (#26)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -1,22 +1,19 @@
 test.help:
 	@echo '    Tests:'
 	@echo ''
-	@echo '        test                    Run all test'
-	@echo '        test.run                Run test of test'
-	@echo '        test.lint               Run all pre-commit'
-	@echo '        test.syntax             Run all syntax in code'
+	@echo '        test                      show help'
+	@echo '        test.all   	             Run all module test'
+	@echo '        test.lint                 Run all pre-commit'
 	@echo ''
 
 test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	@if [ -z "${run}" ]; then \
-		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest"; \
-	elif [ -n "${run}" ]; then \
-		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest tests/${run}"; \
-	fi
+	make test.help
+
+test.all:
+	@echo $(MESSAGE) Running all tests $(END)
+	make test.lint
 
 test.lint:
+	@echo $(MESSAGE) Running validator for pre-commit $(END)
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
-
-test.syntax:
-	@echo $(MESSAGE) Running tests $(END)
