37eda985999aa9618da48112ac2ae5f33bc25874
@slovacus <slovacus@gmail.com>
2021-03-30 14:42:37 -0500

    chore: implement main go (#26)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/luismayta/learn-go
+
+go 1.16
+
+require github.com/luismayta/hello_go v0.0.0-20200217101316-47d9fefaa146
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/luismayta/hello_go/greeting"
+)
+
+func main() {
+	fmt.Printf(greeting.Hello())
+}
