9cf67b98deca5036e144b7ac50429c816950dc6e
@slovacus <slovacus@gmail.com>
2021-04-20 03:31:03 -0500

    fix: validate code go (#29)

--- beginner/sentences/sentences.go
+++ beginner/sentences/sentences.go
@@ -1,13 +1,11 @@
 package main
 
-func simple_if() {
-	name := "Itachi"
-
-	if name == "Itachi" {
+func simpleIf() {
+	if name := "Itachi"; name == "Itachi" {
 		println("I Am Itachi")
 	}
 }
 
 func main() {
-	simple_if()
+	simpleIf()
 }
