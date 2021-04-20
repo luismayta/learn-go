583ab4e9c3eb5c99a72912f63bc307fef9d818a2
Luis Mayta <luis@hadenlabs.com>
2021-04-20 05:08:26 -0500

    fix: errors golangci lint forbidigo (#29)

--- beginner/map/example1.go
+++ beginner/map/example1.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	planetas := make(map[string]string)
-
-	planetas["Han Solo"] = "Corellia"
-	planetas["Luke"] = "Tatoine"
-	fmt.Println("Luke está en:", planetas["Luke"])
-	fmt.Println("Han Solo está en:", planetas["Han Solo"])
-	planetas["Luke"] = "Alderaan"
-	delete(planetas, "Han Solo")
-	fmt.Println("Luke está en:", planetas["Luke"])
-	fmt.Println("Han Solo está en:", planetas["Han Solo"])
-	fmt.Println("Leia Organa está en:", planetas["Leia Organa"])
+	planets := make(map[string]string)
+	planets["Han Solo"] = "Corellia"
+	planets["Luke"] = "Tatoine"
+	fmt.Printf("Luke this in: %s", planets["Luke"])         // nolint
+	fmt.Printf("Han Solo this in: %s", planets["Han Solo"]) // nolint
+	planets["Luke"] = "Alderaan"
+	delete(planets, "Han Solo")
+	fmt.Printf("Luke now this in: %s", planets["Luke"])           // nolint
+	fmt.Printf("Han Solo now this in: %s", planets["Han Solo"])   // nolint
+	fmt.Printf("Leia Organa this in: %s", planets["Leia Organa"]) // nolint
 }
