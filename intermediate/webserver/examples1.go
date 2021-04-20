793f84dff1463eb4014091f53edca317d0e4faa4
Luis Mayta <luis@hadenlabs.com>
2021-04-20 03:31:03 -0500

    fix: validate code go (#29)

--- intermediate/webserver/examples1.go
+++ intermediate/webserver/examples1.go
@@ -18,16 +18,13 @@ func main() {
 
 func helloRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
-	return
 }
 
 func getRequest(w http.ResponseWriter, r *http.Request) {
 	fileRequest := "./" + r.URL.Path
 	http.ServeFile(w, r, fileRequest)
-	return
 }
 
 func getRabbit(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Rabbit Evil")
-	return
 }
