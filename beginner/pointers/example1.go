f857a1231f9f974f7c2bb27042c8b1a046b97b3b
@slovacus <slovacus@gmail.com>
2021-04-23 16:48:03 -0500

    refactor: examples pointers (#29)

--- beginner/pointers/example1.go
+++ beginner/pointers/example1.go
@@ -1,21 +1,6 @@
-package main
+package beginner
 
-import (
-	"fmt"
-)
-
-type Artist struct {
-	Name, Gnre string
-	Songs      int
-}
-
-func newRelease(a Artist) int {
+func newReleaseArtist(a Artist) int {
 	a.Songs++
 	return a.Songs
-}
-
-func main() {
-	me := Artist{Name: "Luis", Gnre: "Balada", Songs: 42}
-	fmt.Printf("%s releases their %dth song\n", me.Name, newRelease(me))
-	fmt.Printf("%s has a total of %d songs", me.Name, me.Songs)
 }
