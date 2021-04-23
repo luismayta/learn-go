99435d6e2e7a40ca69c19e5e20f7d285f078d045
Luis Mayta <luis@hadenlabs.com>
2021-04-23 16:48:03 -0500

    refactor: examples pointers (#29)

---
+++ beginner/pointers/example1_test.go
@@ -0,0 +1,33 @@
+package beginner
+
+import "testing"
+
+func Test_newReleaseArtist(t *testing.T) {
+	type args struct {
+		a Artist
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test basic",
+			args: args{
+				Artist{
+					Name:  "Luis",
+					Gnre:  "Luis",
+					Songs: 10,
+				},
+			},
+			want: 11,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newReleaseArtist(tt.args.a); got != tt.want {
+				t.Errorf("newRelease() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
